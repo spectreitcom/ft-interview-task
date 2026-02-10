0becd0b00c1ca48acae179283e1ff0432166222f
Przemyslaw Chudzinski <p.chudzinski.spectreit@gmail.com>
2026-02-10 20:16:02 +0100

    feat(images): refactor max image size validation logic
    Extract `maxImageSize` constant to `utils` for reuse and replace inline size definition in `ImagesController` validation logic.

--- src/modules/images/presentation/http/images.controller.ts
+++ src/modules/images/presentation/http/images.controller.ts
@@ -35,7 +35,7 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadImageCommand } from '../../application/commands/upload-image.command';
 import { UploadImageBodyDto } from './dto/upload-image-body.dto';
-import { allowedMimeTypeRegex } from '../../shared/utils';
+import { allowedMimeTypeRegex, maxImageSize } from '../../shared/utils';
 import { PROCESSING_STATUS } from '../../domain/value-objects/image-status';
 
 @Controller('images')
@@ -67,7 +67,7 @@ export class ImagesController {
     @UploadedFile(
       new ParseFilePipe({
         validators: [
-          new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 2 }),
+          new MaxFileSizeValidator({ maxSize: maxImageSize }),
           new FileTypeValidator({
             fileType: allowedMimeTypeRegex,
           }),
